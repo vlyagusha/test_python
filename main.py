73d60fe0a0b1296f3d6e73ab16eb016dadf7c52b
vlyagusha <vlyagusha@justclick.ru>
2020-10-27 21:47:48 +0300

    web requests

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import fib
-import rects
-import logging
+import http.client
+import json
+from pprint import pprint
 
-logging.basicConfig(
-    format='%(filename)s[LINE:%(lineno)d]# \
-    %(levelname)-8s [%(asctime)s] %(message)s',
-    level=logging.INFO
-)
+HOST = 'httpbin.org'
 
-logging.info('Entered fails')
-logging.error('Working')
+connection = http.client.HTTPConnection(host=HOST, port=80)
+# connection.request('GET', '/get')
+# response = connection.getresponse()
+# body = response.read().decode()
+# res = json.loads(body)
+# pprint(res)
+
+headers = {
+    'Content-Type': 'application/json'
+}
+my_data = {
+    'key': 'value',
+    'spam': 'eggs',
+    'what?': 42
+}
+request_body = json.dumps(my_data)
+connection.request('POST', '/post', request_body, headers=headers)
+data = json.loads(connection.getresponse().read().decode())
+pprint(data)
+print('Eq?', data['json'] == my_data)
